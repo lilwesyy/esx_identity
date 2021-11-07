12a8eb2c450f00f9deea35a6b6f72195ac2efc65
Kroella <93837375+Kroella@users.noreply.github.com>
2021-11-07 04:15:30 +0100

    Add files via upload
    Added a Swedish translation to locales

---
+++ locales/sv.lua
@@ -0,0 +1,11 @@
+Locales['en'] = {
+  ['show_registration'] = 'visa registreringsmenyn',
+  ['show_active_character'] = 'visa aktiv karaktär',
+  ['delete_character'] = 'ta bort din karaktär och skapa en ny',
+  ['deleted_character'] = 'din karaktär har tagits bort.',
+  ['not_registered'] = 'du har ingen karaktär registrerad.',
+  ['active_character'] = '~b~aktiv karaktär:~s~ %s %s',
+  ['already_registered'] = 'du har redan en registrerad karaktär.',
+  ['failed_identity'] = 'misslyckades med att ställa in din karaktär, försök igen senare eller kontakta serveradministratören!',
+  ['create_a_character'] = 'du måste skapa en karaktär för att kunna spela.'
+}
